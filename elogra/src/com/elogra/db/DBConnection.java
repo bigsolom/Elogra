c4565f5035e128788c3edcbd44a09790963852ae
Mohamed Y. Fayyad <mfayyad@mfayyad.(none)>
2012-03-10 22:19:36 +0200

    DB Tables and query managers

--- elogra/src/com/elogra/db/DBConnection.java
+++ elogra/src/com/elogra/db/DBConnection.java
@@ -2,6 +2,9 @@ package com.elogra.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBConnection {
 	public Connection getConnection(String username, String pass){
@@ -22,5 +25,27 @@ public class DBConnection {
             return null;
         }
 
+	}
+	
+	public boolean executeUpdate(Connection conn, String sql){
+		try {
+			Statement stmt = conn.createStatement();
+			return stmt.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public ResultSet executeQuery(Connection conn, String sql){
+		try {
+			Statement stmt = conn.createStatement();
+			return stmt.executeQuery(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
---
+++ elogra/src/com/elogra/db/TableNames.java
@@ -0,0 +1,10 @@
+package com.elogra.db;
+
+public interface TableNames {
+	static final String ADDRESSES = "addresses";
+	static final String AREAS = "areas";
+	static final String ENTRIES = "entries";
+	static final String HISTORY_FARES = "history_fares";
+	static final String LIVE_FARES = "live_fares";
+	static final String USERS = "users";
+}
