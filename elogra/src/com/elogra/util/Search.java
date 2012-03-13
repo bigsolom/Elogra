94d87dcf1901eb5a9ae8216f8a73c39e21731b0e
Waleed Mowafy <waleedmowafy@Waleeds-MacBook-Pro.local>
2012-03-13 22:19:49 +0200

    Renaming Table names

--- elogra/src/com/elogra/util/Search.java
+++ elogra/src/com/elogra/util/Search.java
@@ -3,7 +3,7 @@ package com.elogra.util;
 import java.util.ArrayList;
 
 import com.elogra.db.DBConnection;
-import com.elogra.db.TableNames;
+import com.elogra.db.DBConstants;
 import com.elogra.model.Comments;
 import com.elogra.model.Result;
 
@@ -13,19 +13,19 @@ public class Search {
 		Result res = new Result();
 		
 		DBConnection dbc = new DBConnection();
-		String sql = "select * from " + TableNames.LIVE_FARES + " where " + TableNames.LIVE_FARES_SRC_ID + " = " + srcID + " and " + TableNames.LIVE_FARES_DEST_ID + " = " + destID;
+		String sql = "select * from " + DBConstants.LIVE_FARES + " where " + DBConstants.LIVE_FARES_SRC_ID + " = " + srcID + " and " + DBConstants.LIVE_FARES_DEST_ID + " = " + destID;
 		DBConnection.QueryResult qr = dbc.executeQuery(sql);
 		
-		res.setLiveFare(qr.getObject(0, TableNames.LIVE_FARES_FARE).toString());
+		res.setLiveFare(qr.getObject(0, DBConstants.LIVE_FARES_FARE).toString());
 		
-		sql = "select * from " + TableNames.HISTORY_FARES + " where " + TableNames.HISTORY_FARES_SRC_ID + " = " + srcID + " and " + TableNames.HISTORY_FARES_DEST_ID + " = " + destID;
+		sql = "select * from " + DBConstants.HISTORY_FARES + " where " + DBConstants.HISTORY_FARES_SRC_ID + " = " + srcID + " and " + DBConstants.HISTORY_FARES_DEST_ID + " = " + destID;
 		qr = dbc.executeQuery(sql);
-		res.setHistoryFare(qr.getObject(0, TableNames.HISTORY_FARES_FARE).toString());
+		res.setHistoryFare(qr.getObject(0, DBConstants.HISTORY_FARES_FARE).toString());
 
 		
 		Comments cmnt = null;
 		ArrayList<Comments> cmntList = new ArrayList<Comments>();
-		sql = "select * from " + TableNames.ENTRIES + " where " + TableNames.ENTRIES_SRC_ID + " = " + srcID + " and " + TableNames.ENTRIES_DEST_ID + " = " + destID;
+		sql = "select * from " + DBConstants.ENTRIES + " where " + DBConstants.ENTRIES_SRC_ID + " = " + srcID + " and " + DBConstants.ENTRIES_DEST_ID + " = " + destID;
 		qr = dbc.executeQuery(sql);
 		////////// get from another view
 		for (int i = 0; i < qr.getRowsCount(); i++) {
@@ -33,22 +33,22 @@ public class Search {
 			//cmnt.setDestStreet(qr.getObject(0, TableNames.ENTRIES_DEST_ADDR_ID).toString());
 			//cmnt.setSrcStreet(qr.getObject(0, TableNames.ENTRIES_SRC_ADDR_ID).toString());
 			cmnt.setUserName("elogra");
-			cmnt.setTraffic(Integer.parseInt(qr.getObject(i, TableNames.ENTRIES_TRAFFIC_STATUS).toString()));
-			cmnt.setFare(qr.getObject(i, TableNames.ENTRIES_FARE).toString());
-			cmnt.setComment(qr.getObject(i, TableNames.ENTRIES_COMMENT).toString());
+			cmnt.setTraffic(Integer.parseInt(qr.getObject(i, DBConstants.ENTRIES_TRAFFIC_STATUS).toString()));
+			cmnt.setFare(qr.getObject(i, DBConstants.ENTRIES_FARE).toString());
+			cmnt.setComment(qr.getObject(i, DBConstants.ENTRIES_COMMENT).toString());
 			
 			
 			//String usrID = qr.getObject(i, TableNames.ENTRIES_USER_ID).toString();
-			String destAddrID = qr.getObject(i, TableNames.ENTRIES_DEST_ADDR_ID).toString();
-			String srcAddrID = qr.getObject(i, TableNames.ENTRIES_SRC_ADDR_ID).toString();
+			String destAddrID = qr.getObject(i, DBConstants.ENTRIES_DEST_ADDR_ID).toString();
+			String srcAddrID = qr.getObject(i, DBConstants.ENTRIES_SRC_ADDR_ID).toString();
 			
-			sql = "select * from " + TableNames.ADDRESSES + " where " + TableNames.ADDRESSES_ID + " = " + srcAddrID;
+			sql = "select * from " + DBConstants.ADDRESSES + " where " + DBConstants.ADDRESSES_ID + " = " + srcAddrID;
 			DBConnection.QueryResult addrQR = dbc.executeQuery(sql);		
-			cmnt.setSrcStreet(addrQR.getObject(0, TableNames.ADDRESSES_TEXT).toString());
+			cmnt.setSrcStreet(addrQR.getObject(0, DBConstants.ADDRESSES_TEXT).toString());
 
-			sql = "select * from " + TableNames.ADDRESSES + " where " + TableNames.ADDRESSES_ID + " = " + destAddrID;
+			sql = "select * from " + DBConstants.ADDRESSES + " where " + DBConstants.ADDRESSES_ID + " = " + destAddrID;
 			addrQR = dbc.executeQuery(sql);		
-			cmnt.setDestStreet(addrQR.getObject(0, TableNames.ADDRESSES_TEXT).toString());
+			cmnt.setDestStreet(addrQR.getObject(0, DBConstants.ADDRESSES_TEXT).toString());
 			
 			cmntList.add(cmnt);
 		}		
