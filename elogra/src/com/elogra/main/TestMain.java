12bb341c700007a9e57fd318f8ba9607abf3fce8
mohamedfayyad <mohamed.fayyad@gmail.com>
2012-03-24 21:36:05 +0200

    DBconnection updated to handle prepared statement(Integer and String)

--- elogra/src/com/elogra/main/TestMain.java
+++ elogra/src/com/elogra/main/TestMain.java
@@ -1,11 +1,20 @@
 package com.elogra.main;
 
+import java.util.ArrayList;
+
 import com.elogra.db.DBConnection;
 
 public class TestMain {
 
 	public static void main(String[] args) {
 		DBConnection dbc = new DBConnection();
+		String s = "";
+		int x = 0;
+		//System.out.println(dbc.getClass().toString());
+		//System.out.println(s.getClass().toString());
+		ArrayList test = new ArrayList();
+		test.add(x);
+		System.out.println(test.get(0).getClass().toString());
 		dbc.closeConnection();
 	}
 
