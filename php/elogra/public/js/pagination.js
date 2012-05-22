31dca25598577c7ba8571fc6a6f6b0bd22bda06d
Eslam Foad <eslam_foad@rayacorp.com>
2012-05-22 02:11:40 +0200

    make pagination script common

---
+++ php/elogra/public/js/pagination.js
@@ -0,0 +1,55 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function(){
+        
+        $(window).scroll(function(){
+            if  ($(window).scrollTop() == $(document).height() - $(window).height()){//only when scrolling down
+                       getMore(); 
+            }
+        });
+                
+        function getMore(){
+            $.ajax({
+                url: moreActionURL,
+                dataType: "json", 
+                data: getPaginationData(),
+                beforeSend:function(){ // Are not working with dataType:'jsonp'
+                    $('.paginated').after("<div align='center'><img  id='loading' src='/img/progressbar.gif' /></div>");
+                },
+                complete: function(){
+                    $('#loading').remove();
+                },
+                success: function(data) {
+                    if(data.html.length){
+                        $(".paginated").append(data.html);
+                        var page = $(".paginated").attr('data-page');
+                        page++;
+                        $(".paginated").attr('data-page',page);
+                    }
+                }
+             });
+        }
+         
+         function getPaginationData(){
+            var page = $(".paginated").attr('data-page');
+            page++;
+            var paginationData = {"page": page};
+//            var pageNumber = {"page": page};
+//            paginationData.push(pageNumber);
+            if(typeof extraPaginationParams !== 'undefined'){//if it is passed
+                for (var key in extraPaginationParams){//add it to the existing parameters
+                    paginationData[key] = extraPaginationParams[key];
+                }
+            }
+            return paginationData;
+         }
+         
+         
+        
+        
+    });
+
+
