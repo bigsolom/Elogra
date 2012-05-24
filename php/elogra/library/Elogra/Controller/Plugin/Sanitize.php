269ebfa0c39950475caf5d3508c26747500e51f6
Waleed Mowafy <waleed.mowafy@gmail.com>
2012-05-25 01:39:04 +0200

    waring fix

--- php/elogra/library/Elogra/Controller/Plugin/Sanitize.php
+++ php/elogra/library/Elogra/Controller/Plugin/Sanitize.php
@@ -17,7 +17,7 @@ class Elogra_Controller_Plugin_Sanitize extends Zend_Controller_Plugin_Abstract{
         parent::preDispatch($request);
         $params = $request->getParams();
         foreach ($params as $key => $value) {
-            $request->setParam($key, filter_var($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+            $request->setParam($key, filter_var($value, FILTER_SANITIZE_SPECIAL_CHARS));
         }
         
     }
