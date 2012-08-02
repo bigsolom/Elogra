14cbc48a61d98fe8738eed2c7854f364814d6f73
Eslam Foad <eslam_foad@rayacorp.com>
2012-08-02 23:35:58 +0200

    increase comments counter on adding

--- php/elogra/application/languages/ar.php
+++ php/elogra/application/languages/ar.php
@@ -74,5 +74,7 @@ return array(
     'default_comment_fady' => 'الطريق فاضي',
     'no_results_found'=> 'محدش قال أجرة المشوار ده قبل كده, اعمل مشوارك و قولنا',
     'contact_us' => 'تواصل معانا',
+    'comments'=>'اصطباحات',
+    'add_your_comment'=> 'قول اصطباحتك',
     
 );
